fd00fa1433709a2d8f5ab34ac7bab8fd36024742
Vibbe00 <vibbe.pakkanen@gmail.com>
2026-03-31 18:21:40 +0300

    debugging dns issue

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,10 +1,7 @@
 import { Pool } from 'pg';
 import { config } from '../config';
 
-export const db = new Pool({
-  connectionString: config.databaseUrl,
-  ssl: { rejectUnauthorized: false }
-});
+export const db = new Pool ({ connectionString: config.databaseUrl });
 
 db.on('error', (err) => {
     console.error("[DB] Unexpected pool error:", err.message);
